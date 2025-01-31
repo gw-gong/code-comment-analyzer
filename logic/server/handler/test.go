6d3dfc39d7867392821fbe7f4ceaffaaea520941
“paipai2001” <3579548357@qq.com>
2025-01-31 23:43:33 +0800

    paipai

--- logic/server/handler/test.go
+++ logic/server/handler/test.go
@@ -67,6 +67,6 @@ func (t *TestXXX) Handle() {
 	// 向响应体写入一条消息
 	_, err = fmt.Fprintln(t.w, "This is a test route handler function. Insert successfully\n"+analyzedData)
 	if err != nil {
-		log.Printf("Error writing response:", err)
+		log.Printf("Error writing response:%v", err)
 	}
 }
