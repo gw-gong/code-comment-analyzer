7dab6f9e04c47d12fadd63281662565a21df58e3
“paipai2001” <3579548357@qq.com>
2025-02-02 09:33:21 +0800

    paipai

--- logic/server/jwt/jwt.go
+++ logic/server/jwt/jwt.go
@@ -17,47 +17,31 @@ type UserClaims struct {
 }
 
 func AuthorizeUserToken(userID uint64, w http.ResponseWriter, sessionManager redis.SessionManager) error {
-	// 创建用户的声明信息
 	userClaims := &UserClaims{
 		UserID: userID,
 	}
-
-	// 生成 JWT 密钥
 	jwtKey := []byte(config.Cfg.JwtKey)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, userClaims)
-
-	// 签名生成 token
 	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
-		// 如果签名失败，返回错误
 		log.Println(err)
 		return fmt.Errorf("error signing token: %w", err)
 	}
-
-	// 将 token 保存到会话中
 	err = sessionManager.SetSession(userID, tokenString)
 	if err != nil {
-		// 如果保存会话失败，返回错误
 		log.Println(err)
 		return fmt.Errorf("error setting session: %w", err)
 	}
-
-	// 设置响应中的 token Cookie
 	http.SetCookie(w, &http.Cookie{
 		Name:  "token",
 		Value: tokenString,
 	})
-
-	// 返回成功响应
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write([]byte("Login successful and token has been set."))
 	if err != nil {
-		// 如果写入响应体失败，返回错误
 		log.Println(err)
 		return fmt.Errorf("error writing response body: %w", err)
 	}
-
-	// 如果没有发生错误，返回 nil
 	return nil
 }
 
